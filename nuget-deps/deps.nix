1d32a4cb2b5e1bcca8cdefa3cd82f458736b63ef
Eric Krall <krall.eric@gmail.com>
2025-08-02 14:49:00 -0400

    add deps.nix

---
+++ nuget-deps/deps.nix
@@ -0,0 +1,452 @@
+[
+{
+  name = "coverlet.collector.6.0.0.nupkg";
+  version = "coverlet.collector.6.0.0.nupkg";
+  sha256 = "sha256-IEmweTMapcPhFHpmJsPXfmMhravYOrWupgjeOvMmQ4o=";
+}
+{
+  name = "mathnet.numerics.5.0.0.nupkg";
+  version = "mathnet.numerics.5.0.0.nupkg";
+  sha256 = "sha256-RHJCVM6OxquJF7n5Mbe/oNbucBbkge6ULcbAczOgmVo=";
+}
+{
+  name = "microsoft.codecoverage.17.8.0.nupkg";
+  version = "microsoft.codecoverage.17.8.0.nupkg";
+  sha256 = "sha256-cv/wAXfTNS+RWEsHWNKqRDHC7LOQSSdFJ1a9cZuSfJw=";
+}
+{
+  name = "microsoft.netcore.platforms.1.1.0.nupkg";
+  version = "microsoft.netcore.platforms.1.1.0.nupkg";
+  sha256 = "sha256-FeM40ktcObQJk4nMYShB61H/E8B7tIKfl9ObJ0IOcCM=";
+}
+{
+  name = "microsoft.netcore.targets.1.1.0.nupkg";
+  version = "microsoft.netcore.targets.1.1.0.nupkg";
+  sha256 = "sha256-0AqQ2gMS8iNlYkrD+BxtIg7cXMnr9xZHtKAuN4bjfaQ=";
+}
+{
+  name = "microsoft.net.test.sdk.17.8.0.nupkg";
+  version = "microsoft.net.test.sdk.17.8.0.nupkg";
+  sha256 = "sha256-uz7QvW+NsVRsp8FR1wjnGEOkUaPX4JyieywvCN6g2+s=";
+}
+{
+  name = "microsoft.testplatform.objectmodel.17.8.0.nupkg";
+  version = "microsoft.testplatform.objectmodel.17.8.0.nupkg";
+  sha256 = "sha256-9TwGrjVvbtyetw67Udp3EMK5MX8j0RFRjduxPCs9ESw=";
+}
+{
+  name = "microsoft.testplatform.testhost.17.8.0.nupkg";
+  version = "microsoft.testplatform.testhost.17.8.0.nupkg";
+  sha256 = "sha256-+CTYFu631uovLCO47RKe86YaAqfoLA4r73vKORJUsjg=";
+}
+{
+  name = "microsoft.win32.primitives.4.3.0.nupkg";
+  version = "microsoft.win32.primitives.4.3.0.nupkg";
+  sha256 = "sha256-mBNDmPXNTW54XLnPAUwBRvkIORFM7/j0D0I2SyQPDEg=";
+}
+{
+  name = "netstandard.library.1.6.1.nupkg";
+  version = "netstandard.library.1.6.1.nupkg";
+  sha256 = "sha256-iNan1ix7RtncGWC9AjAZ2sk70DoxOsmEOgQ10fXm4Pw=";
+}
+{
+  name = "newtonsoft.json.13.0.1.nupkg";
+  version = "newtonsoft.json.13.0.1.nupkg";
+  sha256 = "sha256-K2tSVW4n4beRPzPu3rlVaBEMdGvWSv/3Q1fxaDh4Mjo=";
+}
+{
+  name = "nuget.frameworks.6.5.0.nupkg";
+  version = "nuget.frameworks.6.5.0.nupkg";
+  sha256 = "sha256-ElqfN4CcKxT3hP2qvxxObb4mnBlYG89IMxO0Sm5oZ2g=";
+}
+{
+  name = "runtime.debian.8-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "runtime.debian.8-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-LXUPLX3DJxsU1Pd3UwjO1PO9NM2elNEDXeu2Mu/vNps=";
+}
+{
+  name = "runtime.fedora.23-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "runtime.fedora.23-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-qeSqaUI80+lqw5MK4vMpmO0CZaqrmYktwp6L+vQAb0I=";
+}
+{
+  name = "runtime.fedora.24-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "runtime.fedora.24-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-SrHqT9wrCBsxILWtaJgGKd6Odmxm8/Mh7Kh0CUkZVzA=";
+}
+{
+  name = "runtime.native.system.4.3.0.nupkg";
+  version = "runtime.native.system.4.3.0.nupkg";
+  sha256 = "sha256-ZBZaodnjvLXATWpXXakFgcy6P+gjhshFXmglrL5xD5Y=";
+}
+{
+  name = "runtime.native.system.io.compression.4.3.0.nupkg";
+  version = "runtime.native.system.io.compression.4.3.0.nupkg";
+  sha256 = "sha256-DWnXs4vlKoU6WxxvCArTJupV6sX3iBbZh8SbqfHace8=";
+}
+{
+  name = "runtime.native.system.net.http.4.3.0.nupkg";
+  version = "runtime.native.system.net.http.4.3.0.nupkg";
+  sha256 = "sha256-c556PyheRwpYhweBjSfIwEyZHnAUB8jWioyKEcp/2dg=";
+}
+{
+  name = "runtime.native.system.security.cryptography.apple.4.3.0.nupkg";
+  version = "runtime.native.system.security.cryptography.apple.4.3.0.nupkg";
+  sha256 = "sha256-2IhBv0i6pTcOyr8FFIyfPEaaCHUmJZ8DYwLUwJ+5waw=";
+}
+{
+  name = "runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-Jy01KhtcCl2wjMpZWH+X3fhHcVn+SyllWFY8zWlz/6I=";
+}
+{
+  name = "runtime.opensuse.13.2-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "runtime.opensuse.13.2-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-wyv00gdlqf8ckxEdV7E+Ql9hJIoPcmYEuyeWb5Oz3mM=";
+}
+{
+  name = "runtime.opensuse.42.1-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "runtime.opensuse.42.1-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-zi+b4sCFrA9QBiSGDD7xPV27r3iHGlV99gpyVUjRmc4=";
+}
+{
+  name = "runtime.osx.10.10-x64.runtime.native.system.security.cryptography.apple.4.3.0.nupkg";
+  version = "runtime.osx.10.10-x64.runtime.native.system.security.cryptography.apple.4.3.0.nupkg";
+  sha256 = "sha256-serkd4A7F6eciPiPJtUyJyxzdAtupEcWIZQ9nptEzIM=";
+}
+{
+  name = "runtime.osx.10.10-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "runtime.osx.10.10-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-gybQU6mPgaWV3rBG2dbH6tT3tBq8mgze3PROdsuWnX0=";
+}
+{
+  name = "runtime.rhel.7-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "runtime.rhel.7-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-VsP72GVveWnGUvS/vjOQLv1U80H2K8nZ4fDAmI61Hm4=";
+}
+{
+  name = "runtime.ubuntu.14.04-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "runtime.ubuntu.14.04-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-4yKGa/IrNCKuQ3zaDzILdNPD32bNdy6xr5gdJigyF5g=";
+}
+{
+  name = "runtime.ubuntu.16.04-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "runtime.ubuntu.16.04-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-HmdJhhRsiVoOOCcUvAwdjpMRiyuSwdcgEv2j9hxi+Zc=";
+}
+{
+  name = "runtime.ubuntu.16.10-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "runtime.ubuntu.16.10-x64.runtime.native.system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-pVFUKuPPIx0edQKjzRon3zKq8zhzHEzko/lc01V/jdw=";
+}
+{
+  name = "system.appcontext.4.3.0.nupkg";
+  version = "system.appcontext.4.3.0.nupkg";
+  sha256 = "sha256-yg95LNQOwFlA1tWxXdQkVyJqT4AnoDc+ACmrNvzGiZg=";
+}
+{
+  name = "system.buffers.4.3.0.nupkg";
+  version = "system.buffers.4.3.0.nupkg";
+  sha256 = "sha256-XqZWb4Kd04960h4U9seivjKseGA/YEIpdplfHYHQ9jk=";
+}
+{
+  name = "system.collections.4.3.0.nupkg";
+  version = "system.collections.4.3.0.nupkg";
+  sha256 = "sha256-afY7VUtD6w/5mYqrce8kQrvDIfS2GXDINDh73IjxJKc=";
+}
+{
+  name = "system.collections.concurrent.4.3.0.nupkg";
+  version = "system.collections.concurrent.4.3.0.nupkg";
+  sha256 = "sha256-KMY5DfJnDeIsa13DpqvyN8NkReZEMAFnlmNglVoFIXI=";
+}
+{
+  name = "system.console.4.3.0.nupkg";
+  version = "system.console.4.3.0.nupkg";
+  sha256 = "sha256-Xh3PPBZr0pDbDaK8AEHbdGz7ePK6Yi1ZyRWI1JM6mbo=";
+}
+{
+  name = "system.diagnostics.debug.4.3.0.nupkg";
+  version = "system.diagnostics.debug.4.3.0.nupkg";
+  sha256 = "sha256-fkA79SjPbSeiEcrbbUsb70u9B7wqbsdM9s1LnoKj0gM=";
+}
+{
+  name = "system.diagnostics.diagnosticsource.4.3.0.nupkg";
+  version = "system.diagnostics.diagnosticsource.4.3.0.nupkg";
+  sha256 = "sha256-OFJRb0ygep0Z3yDBLwAgM/Tkfs4JCDtsNhwDH9cd1Xw=";
+}
+{
+  name = "system.diagnostics.tools.4.3.0.nupkg";
+  version = "system.diagnostics.tools.4.3.0.nupkg";
+  sha256 = "sha256-gVOv1SK6Ape0FQhCVlNOd9cvQKBvMxRX9K0JPVi8w0Y=";
+}
+{
+  name = "system.diagnostics.tracing.4.3.0.nupkg";
+  version = "system.diagnostics.tracing.4.3.0.nupkg";
+  sha256 = "sha256-hCETZpHHGVhPYvb4C0fh4zs+8zv4GPoixagkLZjpa9Q=";
+}
+{
+  name = "system.globalization.4.3.0.nupkg";
+  version = "system.globalization.4.3.0.nupkg";
+  sha256 = "sha256-caL0pRmFSEsaoeZeWN5BTQtGrAtaQPwFi8YOZPZG5rI=";
+}
+{
+  name = "system.globalization.calendars.4.3.0.nupkg";
+  version = "system.globalization.calendars.4.3.0.nupkg";
+  sha256 = "sha256-uNOD0EOVFgnS2fMKvMiEtI9aOw00+Pfy/H+qucAQlPc=";
+}
+{
+  name = "system.globalization.extensions.4.3.0.nupkg";
+  version = "system.globalization.extensions.4.3.0.nupkg";
+  sha256 = "sha256-mmJWA27T0GRVuFP9/sj+4TrR4GJWrzNIk2PDrbr7RQk=";
+}
+{
+  name = "system.io.4.3.0.nupkg";
+  version = "system.io.4.3.0.nupkg";
+  sha256 = "sha256-ruynQHekFP5wPrDiVyhNiRIXeZ/I9NpjK5pU+HPDiRY=";
+}
+{
+  name = "system.io.compression.4.3.0.nupkg";
+  version = "system.io.compression.4.3.0.nupkg";
+  sha256 = "sha256-f5PrQlQgj5Xj2ZnHxXW8XiOivaBvfqDao9Sb6AVinyA=";
+}
+{
+  name = "system.io.compression.zipfile.4.3.0.nupkg";
+  version = "system.io.compression.zipfile.4.3.0.nupkg";
+  sha256 = "sha256-WQl+JgWs+GaRMeiahTFUbrhlXIHapzcpTFXbRvAtvvs=";
+}
+{
+  name = "system.io.filesystem.4.3.0.nupkg";
+  version = "system.io.filesystem.4.3.0.nupkg";
+  sha256 = "sha256-vNIYnvlayuVj0WfRfYKpDrhDptlhp1pN8CYmlVd2TXw=";
+}
+{
+  name = "system.io.filesystem.primitives.4.3.0.nupkg";
+  version = "system.io.filesystem.primitives.4.3.0.nupkg";
+  sha256 = "sha256-LMnfg8Vwavs9cMnq9nNH8IWtAtSfk0/Fy4s4Rt9r1kg=";
+}
+{
+  name = "system.linq.4.3.0.nupkg";
+  version = "system.linq.4.3.0.nupkg";
+  sha256 = "sha256-R5uiSL3l6a3XrXSSL6jz+q/PcyVQzEAByiuXZNSqD/A=";
+}
+{
+  name = "system.linq.expressions.4.3.0.nupkg";
+  version = "system.linq.expressions.4.3.0.nupkg";
+  sha256 = "sha256-+3pvhZY7rip8HCbfdULzjlC9FPZFpYoQxhkcuFm2wk8=";
+}
+{
+  name = "system.net.http.4.3.0.nupkg";
+  version = "system.net.http.4.3.0.nupkg";
+  sha256 = "sha256-UoBB7WPDp2Bne/fwxKF0nE8grJ6FzTMXdT/jfsphj8Q=";
+}
+{
+  name = "system.net.primitives.4.3.0.nupkg";
+  version = "system.net.primitives.4.3.0.nupkg";
+  sha256 = "sha256-MY7Z6vOtFMbEKaLW9nOSZeAjcWpwCtdO7/W1mkGZBzE=";
+}
+{
+  name = "system.net.sockets.4.3.0.nupkg";
+  version = "system.net.sockets.4.3.0.nupkg";
+  sha256 = "sha256-il7dr5VT/QWDg/0cuh+4Es2u8LY//+qqiY9BZmYxSus=";
+}
+{
+  name = "system.objectmodel.4.3.0.nupkg";
+  version = "system.objectmodel.4.3.0.nupkg";
+  sha256 = "sha256-gtmRkWP2Kwr3nHtDh0yYtce38z1wrGzb6fjm4v8wN6Q=";
+}
+{
+  name = "system.reflection.4.3.0.nupkg";
+  version = "system.reflection.4.3.0.nupkg";
+  sha256 = "sha256-NQSZRpZLvtPWDlvmMIdGxcVuyUnw92ZURo0hXsEshXY=";
+}
+{
+  name = "system.reflection.emit.4.3.0.nupkg";
+  version = "system.reflection.emit.4.3.0.nupkg";
+  sha256 = "sha256-5LhkDmhy2FkSxulXR+bsTtMzdU3VyyuZzsxp7/DwyIU=";
+}
+{
+  name = "system.reflection.emit.ilgeneration.4.3.0.nupkg";
+  version = "system.reflection.emit.ilgeneration.4.3.0.nupkg";
+  sha256 = "sha256-mKRknEHNls4gkRwrEgi39B+vSaAz/Gt3IALtS98xNnA=";
+}
+{
+  name = "system.reflection.emit.lightweight.4.3.0.nupkg";
+  version = "system.reflection.emit.lightweight.4.3.0.nupkg";
+  sha256 = "sha256-rKx4a9yZKcajloSZHr4CKTVJ6Vjh95ni+zszPxWjh2I=";
+}
+{
+  name = "system.reflection.extensions.4.3.0.nupkg";
+  version = "system.reflection.extensions.4.3.0.nupkg";
+  sha256 = "sha256-mMOCYzUenjd4rWIfq7zIX9PFYk/daUyF0A8l1hbydAk=";
+}
+{
+  name = "system.reflection.metadata.1.6.0.nupkg";
+  version = "system.reflection.metadata.1.6.0.nupkg";
+  sha256 = "sha256-JJfgaPav7UfEh4yRAQdGhLZF1brr0tUWPl6qmfNWq/E=";
+}
+{
+  name = "system.reflection.primitives.4.3.0.nupkg";
+  version = "system.reflection.primitives.4.3.0.nupkg";
+  sha256 = "sha256-5ogwWB4vlQTl3jjk1xjniG2ozbFIjZTL9ug0usZQuBM=";
+}
+{
+  name = "system.reflection.typeextensions.4.3.0.nupkg";
+  version = "system.reflection.typeextensions.4.3.0.nupkg";
+  sha256 = "sha256-4U4/XNQAnddgQIHIJq3P2T80hN0oPdU2uCeghsDTWng=";
+}
+{
+  name = "system.resources.resourcemanager.4.3.0.nupkg";
+  version = "system.resources.resourcemanager.4.3.0.nupkg";
+  sha256 = "sha256-idiOD93xbbrbwwSnD4mORA9RYi/D/U48eRUsn/WnWGo=";
+}
+{
+  name = "system.runtime.4.3.0.nupkg";
+  version = "system.runtime.4.3.0.nupkg";
+  sha256 = "sha256-51813WXpBIsuA6fUtE5XaRQjcWdQ2/lmEokJt97u0Rg=";
+}
+{
+  name = "system.runtime.extensions.4.3.0.nupkg";
+  version = "system.runtime.extensions.4.3.0.nupkg";
+  sha256 = "sha256-wLDHmozr84v1W2zYCWYxxj0FR0JDYHSVRaRuDm0bd/o=";
+}
+{
+  name = "system.runtime.handles.4.3.0.nupkg";
+  version = "system.runtime.handles.4.3.0.nupkg";
+  sha256 = "sha256-KJ5aXoGpB56Y6+iepBkdpx/AfaJDAitx4vrkLqR7gms=";
+}
+{
+  name = "system.runtime.interopservices.4.3.0.nupkg";
+  version = "system.runtime.interopservices.4.3.0.nupkg";
+  sha256 = "sha256-8sDH+WUJfCR+7e4nfpftj/+lstEiZixWUBueR2zmHgI=";
+}
+{
+  name = "system.runtime.interopservices.runtimeinformation.4.3.0.nupkg";
+  version = "system.runtime.interopservices.runtimeinformation.4.3.0.nupkg";
+  sha256 = "sha256-MYpl6/ZyC6hjmzWRIe+iDoldOMW1mfbwXsduAnXIKGA=";
+}
+{
+  name = "system.runtime.numerics.4.3.0.nupkg";
+  version = "system.runtime.numerics.4.3.0.nupkg";
+  sha256 = "sha256-P5jHCgMbgFMYiONvzmaKFeOqcAIDPu/U8bOVrNPYKqc=";
+}
+{
+  name = "system.security.cryptography.algorithms.4.3.0.nupkg";
+  version = "system.security.cryptography.algorithms.4.3.0.nupkg";
+  sha256 = "sha256-tAJvNSlczYBJ3Ed24Ae27a55tq/n4D3fubNQdwcKWA8=";
+}
+{
+  name = "system.security.cryptography.cng.4.3.0.nupkg";
+  version = "system.security.cryptography.cng.4.3.0.nupkg";
+  sha256 = "sha256-u17vy6wNhqok91SrVLno2M1EzLHZm6VMca85xbVChsw=";
+}
+{
+  name = "system.security.cryptography.csp.4.3.0.nupkg";
+  version = "system.security.cryptography.csp.4.3.0.nupkg";
+  sha256 = "sha256-oefdTU/Z2PWU9nlat8uiRDGq/PGZoSPRgkML11pmvPQ=";
+}
+{
+  name = "system.security.cryptography.encoding.4.3.0.nupkg";
+  version = "system.security.cryptography.encoding.4.3.0.nupkg";
+  sha256 = "sha256-Yuge89N6M+NcblcvXMeyHZ6kZDfwBv3LPMDiF8HhJss=";
+}
+{
+  name = "system.security.cryptography.openssl.4.3.0.nupkg";
+  version = "system.security.cryptography.openssl.4.3.0.nupkg";
+  sha256 = "sha256-DL+D2sc2JrQiB4oAcUggTFyD8w3aLEjJfod5JPe+Oz4=";
+}
+{
+  name = "system.security.cryptography.primitives.4.3.0.nupkg";
+  version = "system.security.cryptography.primitives.4.3.0.nupkg";
+  sha256 = "sha256-fnFi7B3SnVj5a+BbgXnbjnGNvWrCEU6Hp/wjsjWz318=";
+}
+{
+  name = "system.security.cryptography.x509certificates.4.3.0.nupkg";
+  version = "system.security.cryptography.x509certificates.4.3.0.nupkg";
+  sha256 = "sha256-MG3V/owDh273GCUPsGGraNwaVpcydupl3EtPXj6TVG0=";
+}
+{
+  name = "system.text.encoding.4.3.0.nupkg";
+  version = "system.text.encoding.4.3.0.nupkg";
+  sha256 = "sha256-GctHVGLZAa/rqkBNhsBGnsiWdKyv6VDubYpGkuOkBLg=";
+}
+{
+  name = "system.text.encoding.extensions.4.3.0.nupkg";
+  version = "system.text.encoding.extensions.4.3.0.nupkg";
+  sha256 = "sha256-vufHXg8QAKxHlujPHHcrtGwAqFmsCD6HKjfDAiHyAYc=";
+}
+{
+  name = "system.text.regularexpressions.4.3.0.nupkg";
+  version = "system.text.regularexpressions.4.3.0.nupkg";
+  sha256 = "sha256-VLCk1D1kcN2wbAe3d0YQM/PqCsPHOuqlBY1yd2Yo+K0=";
+}
+{
+  name = "system.threading.4.3.0.nupkg";
+  version = "system.threading.4.3.0.nupkg";
+  sha256 = "sha256-ZDQ3dR4pzVwmaqBg4hacZaVenQ/3yAF/uV7BXZXjiWc=";
+}
+{
+  name = "system.threading.tasks.4.3.0.nupkg";
+  version = "system.threading.tasks.4.3.0.nupkg";
+  sha256 = "sha256-Z5rXfJ1EXp3G32IKZGiZ6koMjRu0n8C1NGrwpdIen4w=";
+}
+{
+  name = "system.threading.tasks.extensions.4.3.0.nupkg";
+  version = "system.threading.tasks.extensions.4.3.0.nupkg";
+  sha256 = "sha256-X2hQ5j+fxcmnm88Le/kSavjiGOmkcumBGTZKBLvorPc=";
+}
+{
+  name = "system.threading.timer.4.3.0.nupkg";
+  version = "system.threading.timer.4.3.0.nupkg";
+  sha256 = "sha256-pmhslmhQhP32TWbBzoITLZ4BoORBqYk25OWbru04p9s=";
+}
+{
+  name = "system.xml.readerwriter.4.3.0.nupkg";
+  version = "system.xml.readerwriter.4.3.0.nupkg";
+  sha256 = "sha256-QQ8KgU0lu4F5Unh+TbechO//zaAGZ4MfgvW72Cn1hzA=";
+}
+{
+  name = "system.xml.xdocument.4.3.0.nupkg";
+  version = "system.xml.xdocument.4.3.0.nupkg";
+  sha256 = "sha256-rWtdcmcuElNOSzCehflyKwHkDRpiOhJJs8CeQ0l1CCI=";
+}
+{
+  name = "xunit.2.5.3.nupkg";
+  version = "xunit.2.5.3.nupkg";
+  sha256 = "sha256-X4TkN9y/572LnqOX+fiX6dqsqKe/HbAWZq6PhdD0qGM=";
+}
+{
+  name = "xunit.abstractions.2.0.3.nupkg";
+  version = "xunit.abstractions.2.0.3.nupkg";
+  sha256 = "sha256-0D1y/C34iARI96gb3bAOG8tcGPMjx+fMabTPpydGlAM=";
+}
+{
+  name = "xunit.analyzers.1.4.0.nupkg";
+  version = "xunit.analyzers.1.4.0.nupkg";
+  sha256 = "sha256-2m8ikSNvEzQ53rtqhruTpQZpJ+nzI9lhLj0x9w5FKO0=";
+}
+{
+  name = "xunit.assert.2.5.3.nupkg";
+  version = "xunit.assert.2.5.3.nupkg";
+  sha256 = "sha256-+cbNHWwm1no316xMJsdzO7TAV7bIlm3dz8dvKHQ66CU=";
+}
+{
+  name = "xunit.core.2.5.3.nupkg";
+  version = "xunit.core.2.5.3.nupkg";
+  sha256 = "sha256-TEL6tBUxWDU9rVOj4KaxBpL+uZQ/3kcZWVDDueeQkhw=";
+}
+{
+  name = "xunit.extensibility.core.2.5.3.nupkg";
+  version = "xunit.extensibility.core.2.5.3.nupkg";
+  sha256 = "sha256-IjyXgVi/AAnilPsD4IrnqtSrr7Ugu75DguPnI2bVSFs=";
+}
+{
+  name = "xunit.extensibility.execution.2.5.3.nupkg";
+  version = "xunit.extensibility.execution.2.5.3.nupkg";
+  sha256 = "sha256-zN7R4kZGsujpo8aAn4OHdtA0u/r5aeiZaw6A53B69KU=";
+}
+{
+  name = "xunit.runner.visualstudio.2.5.3.nupkg";
+  version = "xunit.runner.visualstudio.2.5.3.nupkg";
+  sha256 = "sha256-GZN1E9rOGorQvVSbGTsmXgJxj2TtJwJxCuo+oh+NEBI=";
+}
+]
